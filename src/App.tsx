4c0ccbd7e4861aaf49d179f190217d469914fe52
unknown <spenceaw@gmail.com>
2023-03-11 12:54:43 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import BarChart from './BarChart';
+
+function App() {
+  return (
+    <div className='App'>
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
