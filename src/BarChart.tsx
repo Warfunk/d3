74323c349441241a97fd676773ecce165c2550a4
unknown <spenceaw@gmail.com>
2023-03-12 14:53:25 -0400

    y axis

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -14,10 +14,16 @@ const BarChart = () => {
       .select(svgRef.current)
       .append('svg')
       .attr('width', width)
-      .attr('height', height);
+      .attr('height', height)
+      .attr('transform', 'translate(25,25)');
 
     const xScale = d3.scaleBand().range([0, width]);
     xScale.domain(dataSet.map((d) => `${d}`)).padding(0.3);
+
+    const yScale = d3.scaleLinear().domain([0, 500]).range([height, 0]);
+
+    const yAxis = d3.axisLeft(yScale);
+    svg.append('g').call(yAxis).attr('transform', 'translate(40,40)');
 
     svg
       .selectAll('rect')
@@ -26,12 +32,15 @@ const BarChart = () => {
       .append('rect')
       .attr('width', xScale.bandwidth())
       .attr('height', (d) => d)
-      .attr('x', (d) => xScale(`${d}`))
+      .attr('x', (d) => xScale(`${d}`) + 25)
       .attr('y', (d) => height - d)
-      .attr('fill', 'blue');
+      .attr('fill', '#5B8E7D');
   }, []);
   return (
-    <svg ref={svgRef} style={{ height: 500, background: 'grey', width: 500 }} />
+    <svg
+      ref={svgRef}
+      style={{ height: 600, background: '#B0BBBF', width: 600 }}
+    />
   );
 };
 
