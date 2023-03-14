07988393908b77b0210dee51300ca06318e5142d
unknown <spenceaw@gmail.com>
2023-03-13 20:25:29 -0400

    fix y axis issues

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -5,9 +5,14 @@ const dataSet = [124, 400, 239, 24, 328, 488];
 
 const BarChart = () => {
   const svgRef = useRef<any>();
-  const width = 500;
-  const height = 500;
-  const padding = 20;
+  const width = 550;
+  const height = 550;
+  const marginRight = 25;
+  const marginLeft = 35;
+  const marginTop = 20;
+  const marginBottom = 25;
+  const svgHeight = height - marginTop - marginBottom;
+  const svgWidth = width - marginLeft - marginRight;
 
   useEffect(() => {
     const svg = d3
@@ -15,33 +20,37 @@ const BarChart = () => {
       .append('svg')
       .attr('width', width)
       .attr('height', height)
-      .attr('transform', 'translate(25,25)');
+      .attr('background', '#B0BBBF');
 
-    const xScale = d3.scaleBand().range([0, width]);
-    xScale.domain(dataSet.map((d) => `${d}`)).padding(0.3);
+    const chartGroup = svg
+      .append('svg')
+      .attr('width', svgWidth)
+      .attr('height', svgHeight)
+      .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
-    const yScale = d3.scaleLinear().domain([0, 500]).range([height, 0]);
+    const xScale = d3.scaleBand().range([0, svgWidth]);
+    xScale.domain(dataSet.map((d) => `${d}`)).padding(0.15);
 
-    const yAxis = d3.axisLeft(yScale);
-    svg.append('g').call(yAxis).attr('transform', 'translate(40,40)');
+    const yScale = d3.scaleLinear().domain([0, 500]).range([svgHeight, 0]);
 
+    const yAxis = d3.axisLeft(yScale).ticks(5);
     svg
+      .append('g')
+      .call(yAxis)
+      .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+
+    chartGroup
       .selectAll('rect')
       .data(dataSet)
       .enter()
       .append('rect')
       .attr('width', xScale.bandwidth())
       .attr('height', (d) => d)
-      .attr('x', (d) => xScale(`${d}`) + 25)
-      .attr('y', (d) => height - d)
+      .attr('x', (d) => xScale(`${d}`))
+      .attr('y', (d) => svgHeight - d)
       .attr('fill', '#5B8E7D');
   }, []);
-  return (
-    <svg
-      ref={svgRef}
-      style={{ height: 600, background: '#B0BBBF', width: 600 }}
-    />
-  );
+  return <svg ref={svgRef} style={{ width: 550, height: 550 }} />;
 };
 
 export default BarChart;
