4dfe778fa8ab475a2944f92d088e3be9c8daad97
unknown <spenceaw@gmail.com>
2023-03-13 21:06:38 -0400

    update chart in progress

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -1,10 +1,19 @@
 import * as d3 from 'd3';
-import { LegacyRef, RefObject, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
-const dataSet = [124, 400, 239, 24, 328, 488];
+const dataSet = [
+  { inches: 44, month: 'November', cm: 112 },
+  { inches: 400, month: 'December', cm: 1016 },
+  { inches: 239, month: 'January', cm: 607 },
+  { inches: 190, month: 'February', cm: 482 },
+  { inches: 328, month: 'March', cm: 833 },
+  { inches: 250, month: 'April', cm: 635 },
+];
 
 const BarChart = () => {
   const svgRef = useRef<any>();
+  const [unit, setUnit] = useState<'inches' | 'cm'>('inches');
+
   const width = 550;
   const height = 550;
   const marginRight = 25;
@@ -13,6 +22,11 @@ const BarChart = () => {
   const marginBottom = 25;
   const svgHeight = height - marginTop - marginBottom;
   const svgWidth = width - marginLeft - marginRight;
+
+  const handleUnitChange = useCallback(() => {
+    return unit === 'inches' ? setUnit('cm') : setUnit('inches');
+  }, []);
+  const unitOption = useMemo(() => (unit === 'inches' ? 'cm' : 'inches'), []);
 
   useEffect(() => {
     const svg = d3
@@ -29,9 +43,12 @@ const BarChart = () => {
       .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
     const xScale = d3.scaleBand().range([0, svgWidth]);
-    xScale.domain(dataSet.map((d) => `${d}`)).padding(0.15);
+    xScale.domain(dataSet.map((d) => `${d[unit]}`)).padding(0.15);
 
-    const yScale = d3.scaleLinear().domain([0, 500]).range([svgHeight, 0]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataSet.map((d) => d[unit]))])
+      .range([svgHeight, 0]);
 
     const yAxis = d3.axisLeft(yScale).ticks(5);
     svg
@@ -45,12 +62,17 @@ const BarChart = () => {
       .enter()
       .append('rect')
       .attr('width', xScale.bandwidth())
-      .attr('height', (d) => d)
-      .attr('x', (d) => xScale(`${d}`))
-      .attr('y', (d) => svgHeight - d)
+      .attr('height', (d) => svgHeight - yScale(d[unit]))
+      .attr('x', (d) => xScale(`${d[unit]}`))
+      .attr('y', (d) => yScale(d[unit]))
       .attr('fill', '#5B8E7D');
-  }, []);
-  return <svg ref={svgRef} style={{ width: 550, height: 550 }} />;
+  }, [unit]);
+  return (
+    <>
+      <button onClick={handleUnitChange}>{`Change to ${unitOption}`}</button>
+      <svg ref={svgRef} style={{ width: 550, height: 550 }} />
+    </>
+  );
 };
 
 export default BarChart;
