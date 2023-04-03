424d62489bec867ec3afd794424319ed2bfc10a7
Alexander Spencer <Alexander.Spencer@bottomline.com>
2023-04-03 09:22:38 -0400

    update to space

--- src/Globe.tsx
+++ src/Globe.tsx
@@ -27,23 +27,27 @@ const Globe = () => {
       .select(svgRef.current)
       .attr('width', width)
       .attr('height', height);
+
     svg
       .append('circle')
       .attr('cx', 200)
       .attr('cy', 200)
       .attr('r', 200)
       .attr('fill', '#00b7e4');
+
     mapGroup.current = svg
       .append('g')
       .attr('class', 'map')
       .attr('width', width)
       .attr('height', height);
+
     graticuleGroup.current = svg
       .append('g')
       .attr('class', 'geo')
       .attr('width', width)
       .attr('height', height);
   }, [data]);
+
   useEffect(() => {
     if (!data) return;
     projection.current = d3
@@ -51,6 +55,7 @@ const Globe = () => {
       .fitSize([width, height], data)
       .rotate(rotate);
     let geoGenerator = d3.geoPath().projection(projection.current);
+
     mapGroup.current
       .selectAll('.path')
       .data(data.features)
@@ -65,6 +70,7 @@ const Globe = () => {
             .attr('stroke-width', '1px'),
         (update) => update.attr('d', geoGenerator)
       );
+
     graticuleGroup.current
       .selectAll('.graticule')
       .data([graticule()])
@@ -81,6 +87,7 @@ const Globe = () => {
         (exit) => exit.remove()
       );
   }, [data, rotate]);
+
   window.requestAnimationFrame(() => {
     setRotate((current) => {
       const new1 = current[0] === 360 ? 0 : current[0] + 0.1;
